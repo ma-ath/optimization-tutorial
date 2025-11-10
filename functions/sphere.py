e324882c0e54a3540b09b04c469582453ee047bc
Matheus Lima <m.mathelima@gmail.com>
2025-11-10 22:19:31 +0900

    Brushing Sphere function

--- functions/sphere.py
+++ functions/sphere.py
@@ -8,8 +8,16 @@ class Sphere(Function):
     or equivalently,
         f(\bm{x}) = \bm{x}^T \bm{x}.
 
-    Global minimum at x_{opt} = [0, 0, ..., 0], f(x_{opt}) = 0.
+    Global minimum at x_{min} = [0, 0, ..., 0], f(x_{min}) = 0.
     """
+
+    @property
+    def global_minimum(self) -> np.ndarray:
+        return np.array([0.0, 0.0])
+    
+    @property
+    def search_domain(self) -> tuple[float, float]:
+        return (-100.0, 100.0)
 
     def __call__(self, x: np.ndarray) -> np.ndarray:
         """Evaluate the Sphere function at a given point.
@@ -23,9 +31,11 @@ class Sphere(Function):
 
 if __name__ == "__main__":
     f = Sphere()
-    x = np.array([[1.0, 1.0, 1.0], 
-                  [0.0, 0.0, 0.0],
-                  [-1.0, 1.0, 1.0]])
+    x = np.random.uniform(f.search_domain[0], f.search_domain[1], (5, 3))
     y = f(x)
     for i in range(x.shape[0]):
+        print(f"Sphere function value at {x[i]}: {y[i]}")
         assert y[i] == f(x[i])
+
+    print(f"Sphere function value at global minimum {f.global_minimum}: {f(f.global_minimum)}")
+    assert np.isclose(f(f.global_minimum), 0.0)
